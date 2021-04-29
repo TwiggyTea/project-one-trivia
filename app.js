a13602e451fee27a87a3728ae80f55627ced41e0
Jarod Torres <me@jarodtorres.com>
2021-04-29 12:21:25 -0400

    MVP

--- app.js
+++ app.js
@@ -1,32 +1,61 @@
+//QUERY SELECTORS
+
 const answersDiv = document.querySelector('#answers')
 const answerA = document.querySelector('#answer-a')
 const answerB = document.querySelector('#answer-b')
 const answerC = document.querySelector('#answer-c')
 const answerD = document.querySelector('#answer-d')
+const scoreBoard = document.querySelector('#score')
+const correctAnswer = document.querySelector('.correct')
 
-
+//QUESTION NUMBER
 let questionNumber = 0
 
-const aAnswers = ["A) pick me, I'm the answer!", '2A']
-const bAnswers = ["B) 3.14159", '2B']
-const cAnswers = ["C) Des Moines", '2C']
-const dAnswers = ["D) A is being ridiculous, the answer is ____", '2D']
+//CURRENT SCORE
+let score = 0
 
+
+//ANSWER ARRAYS
+const aAnswers = ["A) pick me, I'm the answer!<div class=\"correct\">xxx</div>", '2A','test','answer 4']
+const bAnswers = ["B) 3.14159", '2B<div class=\"correct\">xxx</div>','test','answer 4']
+const cAnswers = ["C) Des Moines", '2C','test<div class=\"correct\">xxx</div>','answer 4']
+const dAnswers = ["D) A is being ridiculous, the answer is ____", '2D','test','answer 4<div class=\"correct\">xxx</div>']
+
+//CALLBACK FUNCTIONS
 const nextQuestion = () => {
     questionNumber++
 
     if(aAnswers[questionNumber] == undefined) {
         questionNumber = 0
+        score = -1
+        
     }
 
-    answerA.innerText = aAnswers[questionNumber]
-    answerB.innerText = bAnswers[questionNumber]
-    answerC.innerText = cAnswers[questionNumber]
-    answerD.innerText = dAnswers[questionNumber]
+    answerA.innerHTML = aAnswers[questionNumber]
+    answerB.innerHTML = bAnswers[questionNumber]
+    answerC.innerHTML = cAnswers[questionNumber]
+    answerD.innerHTML = dAnswers[questionNumber]
 }
 
+const incrementScore = () => {
+
+    score++
+    scoreBoard.innerText = `SCORE: ${score}`
+
+}
+
+
+//EVENT LISTENERS
 answersDiv.addEventListener('click', (event) => {
+
+
     if (event.target.classList.contains('answer-item')) {
-        nextQuestion()
+        if (event.target.children.length != 0) {
+            console.log('hello')
+            nextQuestion()
+            incrementScore()
+        } else {
+            nextQuestion()
+        }
     }
 })
